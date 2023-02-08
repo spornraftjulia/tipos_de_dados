15810802b3ad30aad35a2afb0da6bf7c4348c171
spornraftjulia <julia_ramosraft@hotmail.com>
2023-02-08 17:04:20 +0000

    variaveis salvas

--- variaveis.java
+++ variaveis.java
@@ -1,4 +1,28 @@
 public class variaveis {
-
     
-}
+    public static void main(String[] args) {
+
+
+    int idade=17;
+    byte quantidadeBolas = 7;
+    char letra = 'J';
+    boolean vivo = false;
+    double salario = 1302;
+    float salarioMenor = 1212;
+    short alunosPalmital = 10000;
+    long populacaoPalmital = 22000;
+    String nome;
+    nome="Julinha da Etec Palmital";
+
+    System.out.println("Meu nome é: " +nome + ", tenho: "+idade+" anos");
+
+
+    System.out.println(idade);
+    System.out.println(quantidadeBolas);
+    System.out.println(letra);
+    System.out.println(vivo);
+    System.out.println(salario);
+    System.out.println(salarioMenor);
+    System.out.println(alunosPalmital);
+    System.out.println(populacaoPalmital);
+}}
