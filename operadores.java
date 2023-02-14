e699a2ef3021b8a562531cea786b8cade25627a7
spornraftjulia <julia_ramosraft@hotmail.com>
2023-02-14 14:12:46 +0000

    salvando operadores

--- operadores.java
+++ operadores.java
@@ -1,4 +1,12 @@
 public class operadores {
-
+    public static void main(String [] julia){
+        
+        int idade;
+        idade = 16;
+        idade++; 
+        System.out.println("Eu tenho "+ idade +" anos");
+        idade+=10;
+        System.out.println(idade);
+    }
     
 }
